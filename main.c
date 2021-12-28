78ca926179c445e2082ff090c60666437640b959
Andrew Palmer <atp@sdf.org>
2021-12-28 06:34:49 -0600

    Deal board

--- main.c
+++ main.c
@@ -12,6 +12,20 @@ static const char SuitSym[] = "shdc";
 #define SUIT(c) (SuitSym[SUITX(c)])
 
 typedef struct {
+    int id;
+    char text[3];  /* '\0'-terminated string */
+} Card;
+
+Card Card_from_id(int c)
+{
+    Card result = {
+        .id = c,
+        .text = {RANK(c), SUIT(c), '\0'},
+    };
+    return result;
+}
+
+typedef struct {
     int count;
     int cards[52];
 } Deck;
@@ -42,20 +56,50 @@ void Deck_shuffle(Deck *this)
     }
 }
 
-int Deck_next(Deck *this)
+Card Deck_next(Deck *this)
 {
-    int result = this->cards[this->count - 1];
+    int id = this->cards[this->count - 1];
     --this->count;
-    return result;
+    return Card_from_id(id);
+}
+
+void deal_player(Deck *deck, const char *name)
+{
+    Card hand[] = {Deck_next(deck), Deck_next(deck)};
+    printf("%s:\n", name);
+    printf("\t%s %s\n", hand[0].text, hand[1].text);
+}
+
+void deal_board(Deck *deck)
+{
+    Card flop[] = {
+        Deck_next(deck),
+        Deck_next(deck),
+        Deck_next(deck),
+    };
+
+    printf("Flop:\n");
+    printf("\t%s %s %s\n",
+        flop[0].text, flop[1].text, flop[2].text);
+
+    Card turn = Deck_next(deck);
+
+    printf("Turn:\n");
+    printf("\t%s %s %s %s\n",
+        flop[0].text, flop[1].text, flop[2].text, turn.text);
+
+    Card river = Deck_next(deck);
+
+    printf("River:\n");
+    printf("\t%s %s %s %s %s\n",
+        flop[0].text, flop[1].text, flop[2].text, turn.text, river.text);
 }
 
 int main(void)
 {
     Deck *deck = Deck_new();
     Deck_shuffle(deck);
-    while (deck->count) {
-        int card = Deck_next(deck);
-        printf("Card: %c%c (%d)\n", RANK(card), SUIT(card), card);
-    }
+    deal_player(deck, "Hero");
+    deal_board(deck);
     Deck_cleanup(deck);
 }
