165a2cdb4ac87ee1b0c070d4425a46005ed14394
Andrew Palmer <atp@sdf.org>
2021-12-28 06:06:08 -0600

    Shuffle deck

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 static const char RankSym[] = "23456789TJQKA";
 static const char SuitSym[] = "shdc";
@@ -30,6 +31,17 @@ void Deck_cleanup(Deck *this)
     free(this);
 }
 
+void Deck_shuffle(Deck *this)
+{
+    srand(time(NULL));
+    for (int i = 0; i < this->count; ++i) {
+        int newpos = rand() % this->count;
+        int tmp = this->cards[newpos];
+        this->cards[newpos] = this->cards[i];
+        this->cards[i] = tmp;
+    }
+}
+
 int Deck_next(Deck *this)
 {
     int result = this->cards[this->count - 1];
@@ -40,6 +52,7 @@ int Deck_next(Deck *this)
 int main(void)
 {
     Deck *deck = Deck_new();
+    Deck_shuffle(deck);
     while (deck->count) {
         int card = Deck_next(deck);
         printf("Card: %c%c (%d)\n", RANK(card), SUIT(card), card);
