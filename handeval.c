86bfb49120f1ccc8415affb986198528d4dde470
Andrew Palmer <atp@sdf.org>
2022-01-04 01:59:29 -0600

    Move display code

--- handeval.c
+++ handeval.c
@@ -63,12 +63,6 @@ static void _sort_cards(EvalState *state)
         state->cards[highx] = state->cards[i];
         state->cards[i] = tmp;
     }
-
-    printf("Sorted Card IDs:");
-    for (int i = 0; i < state->cardlen; ++i) {
-        printf(" [%d:%c]", state->cards[i], RANK(state->cards[i]));
-    }
-    printf("\n");
 }
 
 static void _count_suits(EvalState *state)
@@ -77,12 +71,6 @@ static void _count_suits(EvalState *state)
         int suitx = SUITX(state->cards[i]);
         ++state->suitc[suitx];
     }
-
-    printf("Suit counts:\n");
-    for (int i = 0; i < ARRAYLEN(state->suitc); ++i) {
-        char flushsym = (state->suitc[i] >= 5) ? '*' : ' ';
-        printf("\t%c%c: %d\n", flushsym, SuitSym[i], state->suitc[i]);
-    }
 }
 
 static void _count_ranks(EvalState *state)
@@ -90,13 +78,6 @@ static void _count_ranks(EvalState *state)
     for (int i = 0; i < state->cardlen; ++i) {
         int rankx = RANKX(state->cards[i]);
         ++state->rankc[rankx];
-    }
-
-    printf("Rank counts:\n");
-    for (int i = 0; i < 13; ++i) {
-        if (!state->rankc[i])
-            continue;
-        printf("\t%c: %d\n", RankSym[i], state->rankc[i]);
     }
 }
 
@@ -194,6 +175,28 @@ straight:
     return EVAL_STRAIGHT | evalbits;
 }
 
+static void _display_counts(EvalState *state)
+{
+    printf("Sorted Card IDs:");
+    for (int i = 0; i < state->cardlen; ++i) {
+        printf(" [%d:%c]", state->cards[i], RANK(state->cards[i]));
+    }
+    printf("\n");
+
+    printf("Suit counts:\n");
+    for (int i = 0; i < ARRAYLEN(state->suitc); ++i) {
+        char flushsym = (state->suitc[i] >= 5) ? '*' : ' ';
+        printf("\t%c%c: %d\n", flushsym, SuitSym[i], state->suitc[i]);
+    }
+
+    printf("Rank counts:\n");
+    for (int i = 0; i < 13; ++i) {
+        if (!state->rankc[i])
+            continue;
+        printf("\t%c: %d\n", RankSym[i], state->rankc[i]);
+    }
+}
+
 static void _display(HandEval eval, const char *trying)
 {
     printf("Trying... %10s: ", trying);
@@ -232,6 +235,8 @@ void evaluate(Hand *hand, Board *board)
     _sort_cards(&state);
     _count_suits(&state);
     _count_ranks(&state);
+
+    _display_counts(&state);
 
     _display(_of_a_kind(&state), "Of A Kind");
     _display(_flush(&state), "Flush");
