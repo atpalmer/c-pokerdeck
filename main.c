463aac57cea4dc44a404ff54790c795ad7e5dc9f
Andrew Palmer <atp@sdf.org>
2022-01-08 01:24:45 -0600

    Calculate top starting hands

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "pokerdeck.h"
 
@@ -251,7 +252,67 @@ void Stats_update(Stats *stats, Player *p, Player *winner)
         ++stats->cardwins[c1][c2];
 }
 
-void play_rounds(int rounds)
+typedef struct {
+    char sym[4];
+    double win_pct;
+} Summary;
+
+void Summary_sort(Summary **first, Summary **last)
+{
+    for (Summary **o = first; o <= last - 1; ++o) {
+        Summary **max = o;
+        for (Summary **i = o + 1; i <= last; ++i) {
+            if ((*i)->win_pct > (*max)->win_pct)
+                max = i;
+        }
+        if (max != o) {
+            Summary *tmp = *max;
+            *max = *o;
+            *o = tmp;
+        }
+    }
+}
+
+Summary **Summary_create(Stats *stats)
+{
+    Summary **result = malloc(sizeof(*result) * 13 * 13);
+
+    for (int c1 = RANK_A; c1 >= 0; --c1) {
+        for (int c2 = RANK_A; c2 >= 0; --c2) {
+            Summary *s = malloc(sizeof *s);
+
+            int low, hi, suited;
+            if (c1 <= c2) {
+                low = c1;
+                hi = c2;
+                suited = 0;
+            } else {
+                hi = c1;
+                low = c2;
+                suited = 1;
+            }
+
+            char sym[] = {RankSym[hi], RankSym[low], suited ? 's' : 'o', '\0'};
+            memcpy(s->sym, sym, ARRAYLEN(sym));
+            s->win_pct = (double)stats->cardwins[c1][c2] / (double)stats->carddeals[c1][c2];
+
+            result[(13 * c1) + c2] = s;
+        }
+    }
+
+    Summary_sort(&result[0], &result[(13 * 13) - 1]);
+
+    return result;
+}
+
+void Summary_destroy(Summary **this)
+{
+    for (int i = 0; i < 13 * 13; ++i)
+        free(this[i]);
+    free(this);
+}
+
+void top_hands(int rounds)
 {
     Stats stats = {0};
 
@@ -266,35 +327,19 @@ void play_rounds(int rounds)
         Game_destroy(game);
     }
 
-    for (int c1 = RANK_A; c1 >= 0; --c1) {
-        for (int c2 = RANK_A; c2 >= 0; --c2) {
-            if (!stats.carddeals[c1][c2])
-                continue;
+    Summary **summary = Summary_create(&stats);
 
-            int low, hi, suited;
-            if (c1 <= c2) {
-                low = c1;
-                hi = c2;
-                suited = 0;
-            } else {
-                hi = c1;
-                low = c2;
-                suited = 1;
-            }
-
-            printf("[%c%c%c]: %.01f%% (%d / %d)\n",
-                RankSym[hi],
-                RankSym[low],
-                suited ? 's' : 'o',
-                ((double)stats.cardwins[c1][c2] * 100.0) / (double)stats.carddeals[c1][c2],
-                stats.cardwins[c1][c2],
-                stats.carddeals[c1][c2]);
-        }
+    printf("Top starting hands for %d simulations:\n", rounds);
+    for (int i = 0; i < 13 * 13; ++i) {
+        Summary *s = summary[i];
+        printf("%d) [%s]: %.01f%%\n", i + 1, s->sym, s->win_pct * 100);
     }
+
+    Summary_destroy(summary);
 }
 
 int main(void)
 {
     srand(time(NULL));
-    play_rounds(1000000);
+    top_hands(1000000);
 }
